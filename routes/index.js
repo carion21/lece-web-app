9847f798331de7683e2f571dc61cffc7e315ad58
Medi G Fulgence <lycorisblue99@gmail.com>
2024-06-26 11:30:43 +0000

    add author page and detais

--- routes/index.js
+++ routes/index.js
@@ -66,8 +66,53 @@ router.get('/book_details/:slug', async (req, res) => {
 });
 
 
-router.get('/author', (req, res) => {
-  res.render('author');
+router.get('/author', async (req, res) => {
+
+  try {
+    // URL de l'API ou chemin du fichier JSON
+    const url = 'http://167.86.106.97:3535/author' // ou './chemin/vers/votre/fichier.json';
+    
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    const jsonData = await response.json();
+    
+    // Extraction des livres à partir de data
+    const authors = jsonData.data;
+    console.log(authors)
+    
+    // Rendu de la page avec les données des livres
+    res.render('author', { authors: authors });
+  } catch (error) {
+    console.error('Erreur lors de la récupération des données :', error);
+    res.status(500).render('error', { message: 'Erreur lors de la récupération des données des livres' });
+  }
+});
+
+router.get('/author_details/:slug', async (req, res) => {
+  try {
+    const { slug } = req.params;
+    
+    // URL de l'API pour récupérer les détails d'un livre spécifique
+    const apiUrl = `http://167.86.106.97:3535/author/by-slug/${slug}`;
+    
+    const response = await fetch(apiUrl);
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    
+    const authorData = await response.json();
+    
+    // Vérifiez si les données du livre sont dans une propriété 'data'
+    const author = authorData.data || authorData;
+    
+    // Rendu de la page avec les données du livre
+    res.render('author_details', { author });
+  } catch (error) {
+    console.error('Erreur lors de la récupération des détails du livre :', error);
+    res.status(500).render('error', { message: 'Erreur lors de la récupération des détails du livre' });
+  }
 });
 
 
